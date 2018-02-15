adbd1e13540c5ffb46dca672b94957661bbd7de5
ANKUL GUPTA <ankulgup@ankulgup-macos-1.corp.adobe.com>
2018-02-15 11:56:09 +0530

    Modified config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 blog = Flask(__name__)
+
+blog.config.from_object(Config)
+
 blog.static_folder = 'static'
 
-blog.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
-blog.secret_key='notnullkey'
+# blog.config['SQLALCHEMY_DATABASE_URI']='sqlite:///posts.db'
+# blog.secret_key='notnullkey'
 
 db=SQLAlchemy(blog)
 
--- app/config.py
+++ app/config.py
@@ -5,4 +5,4 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 class Config(object):
 	SECRET_KEY = os.environ.get("SECRET_KEY") or 'not-a-regular-key'
 	SQLALCHEMY_DATABASE_URI=os.environ.get('DATABSE_URL') or 'sqlite:///' + os.path.join(basedir, 'posts.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS=FALSE
+	SQLALCHEMY_TRACK_MODIFICATIONS=False
