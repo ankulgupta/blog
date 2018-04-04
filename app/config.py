eb2a1ea6534e5b824d8d83f184dfcb079919c74f
ANKUL GUPTA <ankulgup@ankulgup-macos-1.corp.adobe.com>
2018-04-04 17:12:59 +0530

    Image display added to post

--- app/config.py
+++ app/config.py
@@ -6,8 +6,8 @@ class Config(object):
 	SECRET_KEY = os.environ.get("SECRET_KEY") or 'not-a-regular-key'
 	SQLALCHEMY_DATABASE_URI=os.environ.get('DATABSE_URL') or 'sqlite:///' + os.path.join(basedir, 'posts.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS=False
-	UPLOADS_DEFAULT_DEST = '/Users/ankulgup/Documents/img/'
+	UPLOADS_DEFAULT_DEST = 'app/static/img/'
 	UPLOADS_DEFAULT_URL = 'http://localhost:5000/static/img/'
 	 
-	UPLOADED_IMAGES_DEST = '/Users/ankulgup/Documents/img/'
+	UPLOADED_IMAGES_DEST = 'app/static/img/'
 	UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
